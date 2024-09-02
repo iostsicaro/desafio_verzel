bc08b446f5823b4a045ef4ef17b0a8ecc46c981a
Icaro <iostsicaro@gmail.com>
2024-09-02 15:47:36 -0300

    refactoring project and removing previous files

--- books-api-backend/src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const books = require('./controllers/books')
-
-const routes = express();
-
-routes.get('/listarfilmes', books.listarFilmes)
-
-module.exports = routes;
