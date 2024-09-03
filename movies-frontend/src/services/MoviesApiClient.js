2c1cfb43e9000f2fcf724757c8e795409aae68f7
Icaro <iostsicaro@gmail.com>
2024-09-03 13:49:24 -0300

    Creating logic for the requests.

---
+++ movies-frontend/src/services/MoviesApiClient.js
@@ -0,0 +1,39 @@
+const BASE_URL = 'http://localhost:2999/'; // Atualize para sua URL base
+
+const request = async (resource, { method, data, token } = {}) => {
+  const headers = {
+    'Content-type': 'application/json',
+    ...(token && { Authorization: `Bearer ${token}` }),
+  };
+
+  const response = await fetch(`${BASE_URL}${resource}`, {
+    method: method || 'GET',
+    headers,
+    ...(data && { body: JSON.stringify(data) }),
+  });
+
+  return response;
+};
+
+const post = (resource, data, token) => {
+  return request(resource, { method: 'POST', data, token });
+};
+
+const put = (resource, data, token) => {
+  return request(resource, { method: 'PUT', data, token });
+};
+
+const get = (resource, token) => {
+  return request(resource, { method: 'GET', token });
+};
+
+const del = (resource, token) => {
+  return request(resource, { method: 'DELETE', token });
+};
+
+export { 
+    post, 
+    put, 
+    get, 
+    del 
+};
