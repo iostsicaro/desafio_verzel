fab3bf530a96a1b1cadf8522f287497c8e45cc64
Icaro <iostsicaro@gmail.com>
2024-09-04 00:13:27 -0300

    Importing global css for the application.

--- movies-frontend/src/index.jsx
+++ movies-frontend/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Rotas from './Rotas';
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
