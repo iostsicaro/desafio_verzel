276e3caae4b6c1a16e7537815787a37338139021
Icaro <iostsicaro@gmail.com>
2024-09-06 02:47:38 -0300

    adding new logic for the requests

--- movies-api-backend/src/controllers/movies.js
+++ movies-api-backend/src/controllers/movies.js
@@ -3,12 +3,31 @@ const instanceAxios = require('../services/tmdb');
 
 const listMovies = async (req, res) => {
     try {
-        const { data: { results: movies } } = await instanceAxios.get('movie/popular');
-        
-        if (!movies) {
+        const { data: { results: movies } } = await instanceAxios.get('discover/movie');
+
+        if (movies) {
+            for (const movie of movies) {
+                const existingMovie = await knex('movies').where({ title: movie.title }).first();
+
+                if (!existingMovie) {
+                    await knex('movies').insert({
+                        id: movie.id,
+                        title: movie.title,
+                        description: movie.overview,
+                        popularity: movie.popularity,
+                        release_date: movie.release_date,
+                        url_image: movie.poster_path
+                    });
+                };
+            }
+        } else {
             const moviesDatabase = await knex('movies');
 
-            return res.status(200).json(moviesDatabase)
+            if (!moviesDatabase) {
+                return res.status(404).json('Movies not found.');
+            }
+
+            return res.status(200).json(moviesDatabase);
         }
 
         return res.status(200).json(movies);
@@ -19,14 +38,42 @@ const listMovies = async (req, res) => {
     }
 }
 
+const findMovieById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const { data: { results: movie } } = await instanceAxios.get(`movie/${id}`);
+
+        if (!movie) {
+            const findMovie = await knex('movies').where({ id: id });
+
+            if (!findMovie) {
+                return res.status(404).json('Movie not found.');
+            }
+
+            return res.status(200).json(findMovieBy);
+        }
+
+        return res.status(200).json(movie);
+    } catch (error) {
+        const { data: { status_message }, status } = error.response;
+
+        return res.status(status).json(status_message);
+    }
+}
+
 const searchMovies = async (req, res) => {
     const { search } = req.body;
-    
+
     try {
         const { data: { results: movie } } = await instanceAxios.get(`search/movie?query=${search}`);
 
         if (!movie) {
             const searchDatabase = await knex('movies').where('title', 'ilike', `%${search}%`);
+
+            if (!searchDatabase) {
+                return res.status(404).json('Movies not found.');
+            }
 
             return res.status(200).json(searchDatabase);
         }
@@ -40,22 +87,24 @@ const searchMovies = async (req, res) => {
 }
 
 const addFavorites = async (req, res) => {
-    const { id, title, description, popularity, url_image } = req.body;
+    const { user } = req;
+    const { movie_id } = req.params;
+    console.log(movie_id)
 
     try {
-        const verifyingDuplicities = await knex('movies').where('title', 'ilike', `%${title}%`).andWhere({ id: id }).first();
+        const findFavorite = await knex('favorite_movies').where({ users_id: user.id, movies_id: movie_id }).first();
 
-        if (verifyingDuplicities) {
+        if (findFavorite) {
             return res.status(404).json('Favorite already exists.');
         }
 
-        const favorite = await knex('produtos').insert({ id: id, title, description: overview, popularity, url_image: poster_path }).returning('*');
+        const addFavorite = await knex('favorite_movies').insert({ users_id: user.id, movies_id: movie_id });
 
-        if (!favorite)  {
+        if (!addFavorite) {
             return res.status(404).json('Unable to add favorite.');
         }
 
-        return res.status(200).json(favorite);
+        return res.status(200).json('Favorite added successfully.');
     } catch (error) {
         const { data: { status_message }, status } = error.response;
 
@@ -64,19 +113,23 @@ const addFavorites = async (req, res) => {
 }
 
 const removeFavorite = async (req, res) => {
-    const { id } = req.params;
+    const { user } = req;
+    const { movie_id } = req.params;
 
     try {
-        const findMovie = await knex('movies').where({ id: id }).first();
+        const findFavorite = await knex('favorite_movies').where({ users_id: user.id, movies_id: movie_id }).first();
 
-        if (!findMovie) {
-            return res.status(404).json('Movie not found.');
+        if (!findFavorite) {
+            return res.status(404).json('Favorite not found.');
         }
 
-        let removeFavorite = await knex('movies').del().where({ id: id });
-        removeFavorite = await knex('favorite_movies').del().where({ movies_id: id });
+        const removeFavorite = await knex('favorite_movies').del().where({ users_id: user.id, movies_id: movie_id });
 
-        return res.status(200).json('Movie successfully deleted.');
+        if (!removeFavorite) {
+            return res.status(404).json('Favorite not removed.')
+        }
+
+        return res.status(200).json('Favorite successfully deleted.');
     } catch (error) {
         const { data: { status_message }, status } = error.response;
 
@@ -86,6 +139,7 @@ const removeFavorite = async (req, res) => {
 
 module.exports = {
     listMovies,
+    findMovieById,
     searchMovies,
     addFavorites,
     removeFavorite,
