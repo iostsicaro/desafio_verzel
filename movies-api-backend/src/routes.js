f08853eb1489911f3fa1b8061378e27ada8cfc1c
Icaro <iostsicaro@gmail.com>
2024-09-06 02:46:58 -0300

    new routes and new params

--- movies-api-backend/src/routes.js
+++ movies-api-backend/src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const loginMiddleware = require('./middlewares/loginMiddleware');
 const { login } = require('./controllers/login');
 const { registerUser } = require('./controllers/user');
-const { listMovies, addFavorites, removeFavorite, searchMovies } = require('./controllers/movies');
+const { listMovies, findMovieById, addFavorites, removeFavorite, searchMovies } = require('./controllers/movies');
 const { listGenres } = require('./controllers/movieGenres');
 
 const routes = express();
@@ -21,8 +21,9 @@ routes.get('/listgenres', listGenres);
 
 // MOVIES ROUTES
 routes.get('/listmovies', listMovies);
+routes.get('/findmovie/:id', findMovieById)
 routes.get('/searchmovies', searchMovies);
-routes.post('/add-favorite', addFavorites);
-routes.delete('/remove-favorite/:id', removeFavorite);
+routes.post('/addfavorite/:movie_id', addFavorites);
+routes.delete('/removefavorite/:movie_id', removeFavorite);
 
 module.exports = routes;
