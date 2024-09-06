98247e936ba18f028bd18a2f952601169944a703
Icaro <iostsicaro@gmail.com>
2024-09-06 16:47:24 -0300

    new logic for the movies card

--- movies-frontend/src/components/MoviesCard/index.jsx
+++ movies-frontend/src/components/MoviesCard/index.jsx
@@ -1,27 +1,77 @@
 import React, { useState } from 'react';
+import { del, post } from '../../services/MoviesApiClient';
+import useAuth from '../../hooks/useAuth';
 
+import Snackbar from '../../components/Snackbar';
 import IconHeart from '../../assets/icon-heart-black.svg';
 import IconHeartFilled from '../../assets/icon-heart-filled.svg';
 
 import './styles.css';
 
-export default function MoviesCard({ movie, onClick }) {
+export default function MoviesCard({ movie }) {
+    const { token } = useAuth();
     const {
+        id,
         title,
         overview,
         popularity,
         release_date,
         poster_path
     } = movie;
-    
+
     const [isFavorite, setFavorite] = useState(false);
+    const [message, setMessage] = useState('');
+    const [openSnack, setOpenSnack] = useState(false);
 
     function handleFavorite() {
         if (!isFavorite) {
+            addFavorite();
             setFavorite(true);
+            return;
         } else {
+            removeFavorite();
             setFavorite(false);
+            return;
         }
+    }
+
+    async function addFavorite() {
+        try {
+            const response = await post(`addfavorite/${id}`, null, token);
+
+            if (!response.ok) {
+                const msg = await response.json();
+
+                showError(msg);
+                return;
+            }
+            
+            return;
+        } catch (error) {
+            showError(error.message);
+        }
+    }
+
+    async function removeFavorite() {
+        try {
+            const response = await del(`removefavorite/${id}`, token);
+
+            if (!response.ok) {
+                const msg = await response.json();
+
+                showError(msg);
+                return;
+            }
+
+            return;
+        } catch (error) {
+            showError(error.message);
+        }
+    }
+
+    function showError(texto) {
+        setOpenSnack(true);
+        setMessage({ texto, status: 'erro' });
     }
 
     return (
@@ -32,6 +82,7 @@ export default function MoviesCard({ movie, onClick }) {
                         <span className="card-title">{title}</span>
                         <span className="card-description">{overview}</span>
                         <div className="card-popularity">Popularity: {popularity}</div>
+                        <div className="card-popularity">{release_date}</div>
                         <img
                             src={isFavorite ? IconHeartFilled : IconHeart}
                             alt="coração"
@@ -44,6 +95,14 @@ export default function MoviesCard({ movie, onClick }) {
                     </div>
                 </div>
             </div>
+
+            {message && (
+                <Snackbar
+                    message={message}
+                    openSnack={openSnack}
+                    setOpenSnack={setOpenSnack}
+                />
+            )}
         </>
     );
 }
