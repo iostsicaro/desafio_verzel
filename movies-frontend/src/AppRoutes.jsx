8b38e30b859db2f368aa106d35954b0a407726cf
Icaro <iostsicaro@gmail.com>
2024-09-09 02:27:10 -0300

    adding new provides for the routes

--- movies-frontend/src/AppRoutes.jsx
+++ movies-frontend/src/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { MoviesProvider } from './context/MoviesContext';
 import ProtectedRoute from './utils/ProtectedRoutes';
 import Login from './pages/Login';
 import UserRegister from './pages/UserRegister';
@@ -9,16 +10,18 @@ import Dashboard from './pages/Dashboard';
 function AppRoutes() {
     return (
         <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/cadastro" element={<UserRegister />} />
+            <MoviesProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/cadastro" element={<UserRegister />} />
 
-                    <Route element={<ProtectedRoute />}>
-                        <Route path="/movies" element={<Dashboard />} />
-                    </Route>
-                </Routes>
-            </Router>
+                        <Route element={<ProtectedRoute />}>
+                            <Route path="/movies" element={<Dashboard />} />
+                        </Route>
+                    </Routes>
+                </Router>
+            </MoviesProvider>
         </AuthProvider>
     );
 }
