cae3ecd468005ab4f76a825bad0c0f19608743f7
Icaro <iostsicaro@gmail.com>
2024-09-09 20:39:06 -0300

    removing favorite movies shared from the authenticator

--- movies-api-backend/src/routes.js
+++ movies-api-backend/src/routes.js
@@ -4,7 +4,7 @@ const { login } = require('./controllers/login');
 const { registerUser } = require('./controllers/user');
 const { listMovies, listFavorites, addFavorites, removeFavorite, searchMovies } = require('./controllers/movies');
 const { listGenres } = require('./controllers/movieGenres');
-const { createLink, shareLink } = require('./controllers/shareMovies');
+const { userLink, createLink, shareLink } = require('./controllers/shareMovies');
 
 const routes = express();
 
@@ -13,6 +13,9 @@ routes.post('/register', registerUser);
 
 // LOGIN ROUTE
 routes.post('/login', login);
+
+// FAVORITES MOVIES SHARED
+routes.get('/:link_token', shareLink);
 
 // MIDDLEWARE VERIFYING LOGIN
 routes.use(loginMiddleware);
@@ -26,7 +29,7 @@ routes.get('/searchmovies', searchMovies);
 routes.get('/listfavorites', listFavorites);
 routes.post('/addfavorite/:movie_id', addFavorites);
 routes.delete('/removefavorite/:movie_id', removeFavorite);
+routes.get('/userlink', userLink);
 routes.post('/createlink', createLink);
-routes.get('/:link_token', shareLink)
 
 module.exports = routes;
