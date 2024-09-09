e3b813226227fd6b101b2e662173105e3b7e7953
Icaro <iostsicaro@gmail.com>
2024-09-09 02:29:06 -0300

    adding user logic for the authenticator provider

--- movies-frontend/src/hooks/useAuthProvider.jsx
+++ movies-frontend/src/hooks/useAuthProvider.jsx
@@ -4,31 +4,39 @@ import { useLocalStorage } from 'react-use';
 export default function useAuthProvider() {
     const [storedToken, setStoredToken, removeStoredToken] = useLocalStorage('TOKEN', null);
     const [token, setToken] = useState(storedToken);
+    const [storedUser, setStoredUser, removeStoredUser] = useLocalStorage('USER', null);
+    const [user, setUser] = useState(storedUser);
 
-    const login = (newToken) => {
+    const login = (newToken, newUser) => {
         setToken(newToken);
         setStoredToken(newToken);
+        setUser(newUser);
+        setStoredUser(newUser);
     };
 
     const logout = () => {
         setToken(null);
+        setUser(null);
         removeStoredToken();
+        removeStoredUser();
     };
 
-    // useEffect(() => {
-    //     const handleBeforeUnload = () => {
-    //         removeStoredToken();
-    //     };
+    useEffect(() => {
+        const handleBeforeUnload = () => {
+            removeStoredToken();
+            removeStoredUser();
+        };
 
-    //     window.addEventListener('beforeunload', handleBeforeUnload);
+        window.addEventListener('beforeunload', handleBeforeUnload);
 
-    //     return () => {
-    //         window.removeEventListener('beforeunload', handleBeforeUnload);
-    //     };
-    // }, [removeStoredToken]);
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, [removeStoredToken, removeStoredUser]);
 
     return {
         token,
+        user,
         login,
         logout,
     };
