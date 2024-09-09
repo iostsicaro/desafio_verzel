7219a63f6819efa53b1a4e60bb03ece0db40819c
Icaro <iostsicaro@gmail.com>
2024-09-09 20:38:45 -0300

    new logic for the favorites

--- movies-api-backend/src/controllers/shareMovies.js
+++ movies-api-backend/src/controllers/shareMovies.js
@@ -1,6 +1,24 @@
 const knex = require('../connection');
 const crypto = require('crypto');
 
+const userLink = async (req, res) => {
+    const { user } = req;
+
+    try {
+        const link = await knex('share_links').where({ user_id: user.id });
+
+        if (!link) {
+            return res.status(404).json('Link not found');
+        }
+        
+        res.status(200).json(link);
+    } catch (error) {
+        const { data: { status_message }, status } = error.response;
+
+        return res.status(status).json(status_message);
+    }
+}
+
 const createLink = async (req, res) => {
     const { user } = req;
 
@@ -13,14 +31,22 @@ const createLink = async (req, res) => {
 
         const token = crypto.randomBytes(16).toString('hex');
 
+        const checkingUserLinkExistence =  await knex('share_links').where({ user_id: user.id }).first();
+
+        if (checkingUserLinkExistence) {
+            return res.status(400).json('Link already exist');
+        }
+
         const createLink = await knex('share_links').insert({ user_id: user.id, link_token: token });
 
         if (!createLink) {
-            return res.status(404).json('Unable to create link.');
+            return res.status(400).json('Unable to create link.');
         }
 
         const shareLink = {
-            link: `${req.protocol}://${req.get('host')}/sharelink/${token}`,
+            id: user.id,
+            name: user.name,
+            link: `${req.protocol}://${req.get('host')}/${token}`,
             link_token: token
         };
 
@@ -33,25 +59,21 @@ const createLink = async (req, res) => {
 }
 
 const shareLink = async (req, res) => {
-    const { user } = req;
+    const { link_token } = req.params;
 
     try {
-        const findUserToken = await knex('share_links').where({ user_id: user.id }).first();
+        const findUserToken = await knex('share_links').where({ link_token: link_token }).first();
 
         if (!findUserToken) {
             return res.status(404).json('Share link not found');
         }
-
-        const favoriteMovies = await knex('favorite_movies')
-            .where({ users_id: user.id })
-            .join('movies', 'movies.id', '=', 'favorite_movies.movies_id')
-            .select('movies.title', 'movies.description', 'movies.popularity', 'movies.release_date', 'movies.url_image')
-        ;
+        
+        const favoriteMovies = await knex('favorite_movies').where({ users_id: findUserToken.user_id });
 
         if (!favoriteMovies) {
             return res.status(404).json('Favorite movies not found');
         }
-
+        
         res.status(200).json(favoriteMovies);
     } catch (error) {
         const { data: { status_message }, status } = error.response;
@@ -61,6 +83,7 @@ const shareLink = async (req, res) => {
 }
 
 module.exports = {
+    userLink,
     createLink,
     shareLink
 };
