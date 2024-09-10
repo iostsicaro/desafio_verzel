69dcdb007b8e89656dd6f92cb4e5c5b3d736683c
Icaro <iostsicaro@gmail.com>
2024-09-09 22:15:53 -0300

    favorite dashboard component

--- movies-frontend/src/pages/FavoritesDashboard/index.jsx
+++ movies-frontend/src/pages/FavoritesDashboard/index.jsx
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { get } from '../../services/MoviesApiClient';
-import useAuth from '../../hooks/useAuth';
-import useMovies from '../../hooks/useMovies';
 
-import MoviesCard from '../../components/MoviesCard/index';
 import InputSearch from '../../components/InputSearch';
 import NavMenu from '../../components/NavMenu';
 import Snackbar from '../../components/Snackbar';
+import Illustration from '../../assets/illustration-header.svg';
+import HeadImagem from '../../assets/bg-dashboard.jpg';
 
 import './styles.css';
 
 export default function FavoritesDashboard() {
-    const { token, userLink } = useAuth();
     const [movies, setMovies] = useState([]);
+    const [filteredMovies, setFilteredMovies] = useState([]);
     const [search, setSearch] = useState('');
     const [message, setMessage] = useState('');
     const [openSnack, setOpenSnack] = useState(false);
-    const { userFavorites } = useMovies();
+    const { link_token } = useParams();
 
-    async function listOrSearchFavoriteMovies(search) {
-        const url = search ? 'searchmovies' : `${userLink}`;
-
+    async function listOrSearchFavoriteMovies() {
         try {
-            const data = await get(url, search ? { search: search } : {}, token);
+            const data = await get(`share/${link_token}`, {});
             const response = await data.json();
 
             setMovies(response);
+            setFilteredMovies(response);
             return;
         } catch (error) {
             showError(error.message);
@@ -33,9 +32,34 @@ export default function FavoritesDashboard() {
     }
 
     useEffect(() => {
-        listOrSearchFavoriteMovies(search)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [search]);
+        listOrSearchFavoriteMovies()
+    }, [link_token]);
+
+    useEffect(() => {
+        searchFavorites(search);
+    }, [search, movies]);
+
+    function formatDate(release_date) {
+        const date = new Date(release_date);
+
+        if (isNaN(date)) {
+            console.error("Invalid date:", release_date);
+            return "Invalid date";
+        }
+
+        return date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    }
+
+    function searchFavorites(search) {
+        const filtered = movies.filter(movie =>
+            movie.original_title.toLowerCase().includes(search.toLowerCase())
+        );
+        setFilteredMovies(filtered);
+    }
 
     function showError(texto) {
         setOpenSnack(true);
@@ -44,18 +68,46 @@ export default function FavoritesDashboard() {
 
     return (
         <div>
-            <NavMenu />
+            <div>
+                <div
+                    className="imagem-cabecalho"
+                    style={{ backgroundImage: `url(${HeadImagem})` }}
+                />
 
-            <div className="form-search">
-                <InputSearch className={search ? 'form-search-typed' : ''} placeholder={'Search movies'} value={search} setValue={setSearch} onChange={() => 'teste'} />
+                <img className="dash-ilustracao" src={Illustration} alt="Ilustração" />
+
+                <div className="localizar-titulo">
+                    <span className="titulo sombreado">
+                        Friend's Favorite Movies
+                    </span>
+                </div>
             </div>
 
-            <div className={`container-movies ${movies.length > 0 ? '' : 'hide'}`}>
-                {movies.map((movie, index) => (
-                    <MoviesCard
-                        key={index}
-                        movie={movie}
-                    />
+            <div className="form-search">
+                <InputSearch
+                    className={search ? 'form-search-typed' : ''}
+                    placeholder={'Search movies'}
+                    value={search}
+                    setValue={setSearch}
+                />
+            </div>
+
+            <div className={`container-movies ${filteredMovies.length > 0 ? '' : 'hide'}`}>
+                {filteredMovies.map((movie, index) => (
+                    <div key={index} className="card-container">
+                        <div className="card-content">
+                            <div className="text-content">
+                                <span className="card-title">{movie.original_title}</span>
+                                <span className="card-description">{movie.overview ? movie.overview : 'Empty Description'}</span>
+                                <div className="card-popularity">Popularity: {movie.popularity}</div>
+                                <div className="card-release">Release date: {formatDate(movie.release_date)}</div>
+                            </div>
+
+                            <div className="image-container">
+                                <img src={`https://image.tmdb.org/t/p/w500${movie.url_image}`} alt={movie.original_title} className="card-image" />
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
 
